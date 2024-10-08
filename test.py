76f2c4f76e5ac47bc1d9785b1926808089d8039b
SuperHacker UEFI <zhuoshizhang@hotmail.com>
2024-10-08 15:29:25 +0900

    Improve Code

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import torch
 import cv2
 import torch
-from torch import nn
-from torch.autograd import Variable
 from config import MyConfigs
 from datasets import *
 from utils.utils import accuracy
@@ -15,9 +13,9 @@ def evaluate(test_loader,model,criterion):
 
     for img, label in test_loader:
         target_test = torch.tensor(label).cuda()
-        output_test = model(img.cuda())
-        loss = criterion(output_test, target_test)
-        top1_test = accuracy(output_test, target_test, topk=(1,))
+        y = model(img.cuda())
+        loss = criterion(y, target_test)
+        top1_test = accuracy(y, target_test, topk=(1,))
         sum += 1
         test_loss_sum += loss.data.cpu().numpy()
         test_top1_sum += top1_test[0].cpu().numpy()[0]
